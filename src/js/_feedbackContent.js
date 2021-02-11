84bd44bc67171a9b17d08061e208b23f576090d9
Gaurav <gaurav.gj93@gmail.com>
2021-02-11 16:16:10 +0530

    merging with font

--- src/js/_feedbackContent.js
+++ src/js/_feedbackContent.js
@@ -47,7 +47,7 @@ Vue.component('feedback-content', {
                             </div>
 
                             <div class="feedback-block">
-                                <textarea id="feedback_comment" name="feedback_comment" :data-info="JsonData.feedbackID" @input="handleInput(JsonData.feedbackID, $event)" class="feedback-input" rows="4" cols="20">{{JsonData.feedbackValue}}</textarea>
+                                <textarea id="feedback_comment" name="feedback_comment" :data-info="JsonData.feedbackID" @input="handleInput(JsonData.feedbackID, $event)" :placeholder="JsonData.feedbackPlaceHolder" class="feedback-input" rows="4" cols="20">{{JsonData.feedbackValue}}</textarea>
                             </div>
                             
                         </div>
