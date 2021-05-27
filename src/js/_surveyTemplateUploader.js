e4e535010ad8d880525836ed7d9a8521272e8689
amit-ojha-e2e <amit.ojha@e2eresearch.com>
2021-05-27 14:22:08 +0530

    proloader added

--- src/js/_surveyTemplateUploader.js
+++ src/js/_surveyTemplateUploader.js
@@ -11,6 +11,62 @@ Vue.component('survey-template-uploader', {
         };
     },
     template:`<div class="assessment-intro">  
+    <!--preloader-->
+    <div class="pre-loading-panel" id="pre-loading-panel">
+    <div class="loading-animation">
+      <img src=https://e2eresearch.com/swteam/McKinsey/Assessment-app/v2/mck-logo.svg alt="" class="logo">
+      <svg viewBox="-25 -25 110 70" preserveAspectRatio>
+          <circle fill="#fff" stroke="none" cx="6" cy="25" r="6">
+          <animateTransform 
+             attributeName="transform" 
+             dur="1s" 
+             type="translate" 
+             values="0 15 ; 0 -15; 0 15" 
+             repeatCount="indefinite" 
+             begin="0.1"/>
+          <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite"
+            begin="0.1"/>  
+        </circle>
+        <circle fill="#fff" stroke="none" cx="30" cy="25" r="6">
+          <animateTransform 
+             attributeName="transform" 
+             dur="1s" 
+             type="translate" 
+             values="0 10 ; 0 -10; 0 10" 
+             repeatCount="indefinite" 
+             begin="0.2"/>
+          <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite" 
+            begin="0.2"/>  
+        </circle>
+        <circle fill="#fff" stroke="none" cx="54" cy="25" r="6">
+          <animateTransform 
+             attributeName="transform" 
+             dur="1s" 
+             type="translate" 
+             values="0 5 ; 0 -5; 0 5" 
+             repeatCount="indefinite" 
+             begin="0.3"/>
+              <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite" 
+            begin="0.3"/>
+        </circle>
+      </svg>
+    </div>
+  </div>
+  <!--preloader end-->
+
+
 
     <!--popup code here-->
     <div class="popup-layer" v-if="showPopup">
